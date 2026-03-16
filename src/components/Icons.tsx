bf57c2ffd1afc3f4b838e7629027daea743d515d
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-16 22:02:09 +0500

    Add src/components/Icons.tsx

---
+++ src/components/Icons.tsx
@@ -0,0 +1,27 @@
+export function ChevronLeftIcon() {
+  return (
+    <svg
+      className="w-5 h-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+    </svg>
+  );
+}
+
+export function ChevronRightIcon() {
+  return (
+    <svg
+      className="w-5 h-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+    </svg>
+  );
+}
